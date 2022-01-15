05f36108ea5111016f8027e9ac75271db09ce642
Te-Wei Tsai <twtsaia@gmail.com>
2022-01-15 16:57:40 -0300

    Refactor the command structure.

--- include/interface/commandStructure.h
+++ include/interface/commandStructure.h
@@ -7,23 +7,22 @@
 #define LENGTH_CMD_STATUS_REASON 50
 
 typedef struct __attribute__((__packed__)) _headerStructure {
-    // Synchronization pattern used to differentiate the command is from the DDS
-    // or GUI (enum: SyncPattern)
-    unsigned short syncpattern;
-    // Frame ID used by the LabVIEW EUI to identify the kind of incoming TCP/IP
-    // packet, and decide the size of packet (in byte) to decode the message
-    unsigned short frameId;
+    // Frame ID to identify the kind of TCP/IP packet.
+    // (enum: 'FrameId')
+    unsigned int frameId;
     // Counter of the packet, the value should increase by 1 after each time
     // of filling the data of TCP/IP packet
     unsigned int counter;
-    // TAI (International Atomic Time) time in second (as provided by CLOCK_TAI)
+    // TAI (International Atomic Time) time in second
     time_t tv_sec;
     // TAI (International Atomic Time) time fraction in nanosecond
     long tv_nsec;
 } headerStructure_t;
 
 typedef struct __attribute__((__packed__)) _commandStreamStructure {
-    unsigned short syncpattern;
+    // Commander of command (enum: 'Commander').
+    unsigned int commander;
+    // Counter of command
     unsigned int counter;
     // Command name
     unsigned int cmd;
@@ -39,7 +38,7 @@ typedef struct __attribute__((__packed__)) _commandStreamStructure {
 typedef struct __attribute__((__packed__)) _commandStatusStructure {
     // Header of packet
     headerStructure_t header;
-    // Command status (enum: CmdStatus)
+    // Command status (enum: 'CmdStatus')
     unsigned int cmdStatus;
     // Estimated duration of command execution in second. The value of 0 means
     // the command excution is supposed to be fast instead of "zero" time.
@@ -49,22 +48,28 @@ typedef struct __attribute__((__packed__)) _commandStatusStructure {
 } commandStatusStructure_t;
 
 typedef enum {
-    // Many compilers assign the first element in an enumerated type to the
-    // value 0. This could be used for unknown sync pattern.
-    SyncPattern_Unknown = 0,
-    // Sync pattern of GUI
-    SyncPattern_GUI = 0x4444,
-    // Sync pattern of DDS
-    SyncPattern_DDS = 0x5555,
-} SyncPattern;
+    // Controller internal use
+    Commander_Self = 0,
+    // Graphical user interface
+    Commander_GUI = 1,
+    // Commandable SAL component
+    Commander_CSC = 2,
+} Commander;
 
 typedef enum {
-    // Unkown status
-    CmdStatus_Unknown = 0,
-    // Command is OK to pass to the Simulink model
-    CmdStatus_OK = 1,
-    // Command is not OK to pass to the Simulink model
-    CmdStatus_NotOK = 2,
+    // Command is OK
+    CmdStatus_OK = 0,
+    // Command is not OK
+    CmdStatus_NotOK = 1,
 } CmdStatus;
+
+typedef enum {
+    // Telemetry
+    FrameId_Tel = 0,
+    // Command status
+    FrameId_CmdStatus = 1,
+    // Configuration
+    FrameId_Config = 2,
+} FrameId;
 
 #endif // COMMANDSTRUCTURE_H
